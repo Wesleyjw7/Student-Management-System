fe352bd3899db21b72a5dbdb72672917731803c8
Wesleyjw7 <124423440+Wesleyjw7@users.noreply.github.com>
2025-03-08 18:49:03 +0530

    Update settings.py

--- smsproject/settings.py
+++ smsproject/settings.py
@@ -78,12 +78,8 @@ WSGI_APPLICATION = 'smsproject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'smsproject',
-        'USER': 'postgres',
-        'PASSWORD':'Mango@123',
-        'HOST':'localhost',
-        'PORT':'5433',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
